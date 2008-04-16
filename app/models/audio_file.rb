7ed613c9bce7974132635d042b879ec07a1614ec
Justin Weiss <justin@uberweiss.net>
2008-04-16 16:40:16 -0700

    Moved upload link

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -5,7 +5,6 @@ class AudioFile < ActiveRecord::Base
   has_many :audio_queues, :dependent => :destroy
   has_attachment :content_type => 'audio/mpeg', :storage => :file_system, :path_prefix => 'public/music', :size => 500.kilobytes..10.megabytes
   validates_as_attachment
-  validates_uniqueness_of :filename
   
   def tag
     @tag ||= ID3Lib::Tag.new(self.full_filename)
@@ -20,7 +19,6 @@ class AudioFile < ActiveRecord::Base
   end
   
   after_attachment_saved do |record|
-    debugger
     record.artist = record.convert_if_necessary(record.tag.artist)
     record.album = record.convert_if_necessary(record.tag.album)
     record.title = record.convert_if_necessary(record.tag.title)
